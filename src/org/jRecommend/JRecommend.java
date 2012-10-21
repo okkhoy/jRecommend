f1a250d5b0c6b6d5695ebbee95029ad8b0cf2e37
akshay narayan <okkhoy@gmail.com>
2012-10-21 11:52:24 +0800

    First commit

---
+++ src/org/jRecommend/JRecommend.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jRecommend;
+
+/**
+ *
+ * @author akshay
+ */
+public class JRecommend {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello!!");
+        System.out.println("Adding a new line to test!");
+    }
+}
